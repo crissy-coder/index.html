bef5144884d9ae6be273a84c43b9f0d05344e58d
kommanavikas <kommana02@gmail.com>
2023-08-11 22:23:30 +0530

    12 seperate projects in one file

---
+++ discount.js
@@ -0,0 +1,150 @@
+
+                                               discount problem
+
+// let total = 2000;
+// let discount = 20;
+
+
+// let finalbill = 2000-20/100*total
+// console.log(finalbill)
+
+                                        SIMPLE LOGIN FORM
+// let Username = "admin";
+// let Password = 1234;
+
+
+// if(Username == "admiin"){
+//     if(Password =="1234"){
+//         console.log("login Successfully")
+//     }
+//     else{
+//         console.log("invalid deatils")
+//     }
+// }
+// else{
+//     console.log("please correct username")
+// }
+                                simple convertio INDIAN  RUPEE TO USD DOLLAR
+
+// let rupee = 850 ;
+// let dollar = 82.6175;
+
+// let converstion = rupee/dollar;
+// console.log(converstion)
+             
+                                CHECKING TOTAL BILL FOR DIFFERENT RATES
+// let adulticket = 100;
+// let childticket = 200;
+// let seniorticket = 100;
+
+// let numberofadults = 2
+// let numberofchild= 3;
+// let numberofsernior =1;
+
+// let subtotal = numberofadults*adulticket + numberofchild*childticket +numberofsernior*seniorticket;
+// console.log(subtotal)
+ 
+                                SWITCH CASE EXAMPLE 
+ 
+// let standard = "3-5 days";
+// let express = "1-2 days";
+// let overnight = "nextday Moring";
+
+// switch(overnight){
+//     case (standard):
+//         console.log("your delivey will be in 3-5 days thank you");
+    
+//     break;
+   
+//     case (express):
+//     console.log("your expected delevery will be in 1-2 days thank you");
+//     break;
+   
+//     case (overnight):
+//     console.log("Great Your Delevery will be in Moring First Hour THANK YOU!")
+//     break;
+   
+//     default:
+//     console.log("we unforcahenateely won`t be delevering at your location, furthur we will approch YOU Thank you!");
+// }
+               
+                                          matching datatype with given value
+
+                 
+// let Username = "kommanavikas";
+// let email = "kommana03@gmail.com";
+// let phonenumber = 9638527412;
+
+// if(typeof Username !== "string"){
+//     console.log("check the username datatype");
+// }
+//     else if(typeof email !== "string"){
+//         console.log("check the mail datatype");
+//     }
+//         else if(typeof phonenumber !== "number"){
+//             console.log("check the mobile number");
+//          }
+// else{
+//     console.log("checked the data type");
+// }
+
+
+                      PRINTING LIST OF ELEMNET IN ARRAY
+
+// let listitems =["pen","box","chocolates","charamile","drinks"];
+// let i
+
+// for(i = 0; i< listitems.length;i++){
+//     console.log(listitems[i]);
+// }
+
+                              printing first 10 numbers
+// let i = 0; 
+// while(i  <= 10){
+//     console.log(i);
+//     i++;
+// }
+                            printing first 10 numbers in revese order 
+// let i = 9;
+// while(i >= 0){
+//    console.log(i);
+//    i--;
+// }
+
+                     printing selected datatype form list of array with different datatypes
+
+// const container =[22,55,"vikas", "sai",1.25,"gero"];
+
+// let container1 = container.length;
+// let i;
+// for(i = 0; i < container1; i++){
+//   // console.log(container[i]); 
+//    if(typeof container[i] === "string")
+//     console.log(`yea finally got it man ${container[i]}`)
+// }
+  
+
+                 printing only even numbers
+                  note: for reminder use percentile (%) for division use (/) slash
+
+
+// let num = [1,2,3,4,5,6,7,8,9,10];
+// let i;
+//  for(i = 0; i < num.length; i++){
+   
+//    if(num[i]%2 == 0){
+//       console.log(`${num[i]} is the even number`);
+      
+//    }            
+//  }
+
+            printing positive number from -ve and +ve  integers
+
+// let arr = [1,3,5,-5,9,-6,-9,-2];
+
+// for (let i = 0; i < arr.length; i++){
+//    if(arr[i] < 0){
+//       continue;
+//    }
+//    console.log(arr[i]);
+// }
---
+++ readme.md
@@ -0,0 +1,4 @@
+in this file total 12 small projects based on opearations 
+
+please run the programe according to divided section 
+
